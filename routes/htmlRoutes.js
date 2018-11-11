d8a1bc3cc92107fd70ae701ce53eb92c2483014a
ncortes12 <ncortes1415@gmail.com>
2018-11-11 09:09:03 -0700

    index and resume page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,10 +6,16 @@ module.exports = function (app){
     app.get("/contact", function(req,res){
         res.sendFile(path.join(__dirname, "/../views/contact.html"))
     });
-    app.get("/portfolio", function(req,res){
-        res.sendFile(path.join(__dirname, "/../views/portfolio.html"))
+    app.get("/research", function(req,res){
+        res.sendFile(path.join(__dirname, "/../views/research.html"))
     });
-    app.get("/resume", function(req,res){
-        res.sendFile(path.join(__dirname, "/../views/resume.html"))
+    app.get("/cv", function(req,res){
+        res.sendFile(path.join(__dirname, "/../views/cv.html"))
+    })
+    app.get("/resources", function(req,res){
+        res.sendFile(path.join(__dirname, "/../views/resources.html"))
+    })
+    app.get("/people", function(req,res){
+        res.sendFile(path.join(__dirname, "/../views/people.html"))
     })
 }
